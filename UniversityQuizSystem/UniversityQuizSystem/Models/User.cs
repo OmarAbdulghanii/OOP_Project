c165d9839e17549f47b54c37f198a7b7a910474c
Omar Abdulghani <omartaman33@gmail.com>
2026-04-29 23:13:43 +0300

    User.cs

--- UniversityQuizSystem/UniversityQuizSystem/Models/User.cs
+++ UniversityQuizSystem/UniversityQuizSystem/Models/User.cs
@@ -1,9 +1,8 @@
 namespace UniversityQuizSystem.Models
 {
-    // Base class: User (Demonstrates base class in Inheritance)
+   
     public class User
     {
-        // Properties
         public string Name { get; set; }
         public UserRole Role { get; set; }
         public StudyYear Year { get; set; }
@@ -12,14 +11,13 @@ namespace UniversityQuizSystem.Models
         public ClassType ClassType { get; set; }
         public int SectionNumber { get; set; }
 
-        // Constructor
         public User(string name, UserRole role)
         {
             Name = name;
             Role = role;
         }
 
-        // Virtual method - can be overridden by subclasses
+
         public virtual string GetInfo()
         {
             return $"Name: {Name}, Role: {Role}";
