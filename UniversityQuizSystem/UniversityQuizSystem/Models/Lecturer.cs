0db62018a82ca7803492825742589d4a8b7327fe
Omar Abdulghani <omartaman33@gmail.com>
2026-04-29 23:09:56 +0300

    Lecturer.cs

--- UniversityQuizSystem/UniversityQuizSystem/Models/Lecturer.cs
+++ UniversityQuizSystem/UniversityQuizSystem/Models/Lecturer.cs
@@ -1,17 +1,17 @@
 namespace UniversityQuizSystem.Models
 {
-    // Lecturer class: inherits from User (Demonstrates Inheritance)
+    
     public class Lecturer : User
     {
         public string Department { get; set; }
 
-        // Constructor calls base class constructor
+  
         public Lecturer(string name) : base(name, UserRole.Lecturer)
         {
             Department = "General";
         }
 
-        // Override base method
+        
         public override string GetInfo()
         {
             return $"Lecturer: {Name} | Department: {Department}";
